75ee3925404613e80a8da2cb70cab278de975a99
Rosareven <rosareven@gmail.com>
2021-07-18 16:29:49 +0100

    Moved all app requires to requires.rb

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,3 @@
-require "smaug/maw/lib/maw.rb"
-require "smaug/draco/lib/draco.rb"
 require "smaug.rb"
 require "app/requires.rb"
 
--- app/requires.rb
+++ app/requires.rb
@@ -1,1 +1,5 @@
+require "app/components/index.rb"
+require "app/entities/index.rb"
+require "app/systems/index.rb"
+require "app/worlds/index.rb"
 require "app/game.rb"
